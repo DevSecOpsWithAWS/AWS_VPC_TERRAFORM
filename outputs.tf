f4a3c4675aa3cb73242607b872717d6e3ab09a08
DevSecOpsWithAWS <choudareddy1234@gmail.com>
2025-02-21 10:32:24 +0530

    VPC Module develpoment

--- outputs.tf
+++ outputs.tf
@@ -6,3 +6,19 @@
 #   value = aws_subnet.public
   
 # }
+
+output "vpc_id" {
+  value = aws_vpc.expense_vpc.id
+  
+}
+ output "public_subnet_id" {
+  value = aws_subnet.public[*].id
+ }
+
+  output "private_subnet_id" {
+  value = aws_subnet.private[*].id
+ }
+
+  output "database_subnet_id" {
+  value = aws_subnet.database[*].id
+ }
